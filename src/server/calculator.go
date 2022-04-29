8702a2f05e3a7e28b40a3b6bbc93495515c2a157
SouvikEndor <souvik@endor.ai>
2022-04-29 11:49:37 -0700

    Add subtract functionality

--- src/server/calculator.go
+++ src/server/calculator.go
@@ -9,3 +9,8 @@ func add(a int, b int) int {
 	fmt.Printf("Adding : %d and %d", a, b)
 	return a + b
 }
+
+func subtract(a int, b int) int {
+	fmt.Printf("Subtracting : %d from %d ", a, b)
+	return a - b
+}
