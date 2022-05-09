8a6e4e45a23d49927da2d263c0961c7584c5f7be
Alexandre Wilhelm <alexandre@endor.ai>
2022-05-09 16:10:02 -0700

    Fixed: lower capital in log

--- src/server/calculator.go
+++ src/server/calculator.go
@@ -21,12 +21,12 @@ func multi(a int, b int) int {
 }
 
 func multiply(a int, b int) int {
-	fmt.Printf("Multiplying : %d and %d ", a, b)
+	fmt.Printf("multiplying : %d and %d ", a, b)
 
 	return a * b
 }
 
 func divide(a int, b int) int {
-	fmt.Printf("Dividing : %d by %d", a, b)
+	fmt.Printf("dividing : %d by %d", a, b)
 	return a / b
 }
